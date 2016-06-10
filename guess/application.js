ad150a2e714d50203d758cebe95b0d32bbe43926
chevelf4346 <chevelf4346@kippnyccp.org>
2016-06-10 10:45:02 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	//create click event for submit button
-
-
-
+	$("#submit").click(function(){
+		var user_guess = $("input[name=first_name]").val();
+		if (user_guess==random_num) {
+			alert("Congrats!");
+		} else if (user_guess<random_num){
+			alert("Guess Higher");
+		} else {
+			alert("Guess Lower");
+		}
+	})
 	//create click event for clear button
 
 
